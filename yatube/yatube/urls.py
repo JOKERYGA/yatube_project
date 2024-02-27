00028c054beec6fe30f37c212a773b0424e00397
JOKERYGA <vlad.gluhovskiy_18376@mail.ru>
2024-02-27 19:02:31 +0700

    Добавлены теги, перед добавлением css и статики

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -14,10 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-# импорт include позволит использовать адреса, включенные в приложения
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('posts.urls')),
+    path('', include('posts.urls', namespace='posts')),
     path('admin/', admin.site.urls),
 ]
