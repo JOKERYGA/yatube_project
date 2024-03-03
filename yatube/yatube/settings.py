be1055602abb775188cb3e9cf79c2fc101766f0a
JOKERYGA <vlad.gluhovskiy_18376@mail.ru>
2024-03-03 21:00:25 +0700

    I have set up links to group entries and icons

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -31,12 +31,12 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.admin', #админка сайта
+    'django.contrib.auth', #Система аутентификации
+    'django.contrib.contenttypes', #Фреймворк для типов контента
+    'django.contrib.sessions', #Фреймворк сеанса
+    'django.contrib.messages', #Фреймворк для обмена сообщениями
+    'django.contrib.staticfiles', #Фреймворк для управления статическими файлами
     'posts.apps.PostsConfig',
 ]
 
@@ -121,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
